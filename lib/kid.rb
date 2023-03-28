df375caf9f6d1de196c61528d200a1d6b3addddc
Beau <beaulazear@gmail.com>
2023-03-27 21:05:11 -0400

    commit tree f890c4138f20f46e8c1fedf94e180371153e7d8d

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Kid
+    include FancyDance::InstanceMethods
+    extend FancyDance::ClassMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
